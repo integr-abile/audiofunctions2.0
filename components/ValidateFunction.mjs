a99704de4d7d536a28877f7410eea5a8967a2b2a
Niccolò Musso Barcucci <niccolo.mussobarcucci@gmail.com>
2023-02-26 12:21:55 +0100

    added abs and minor bugfix
    -minor bugfix: bisogna usare replaceAll, altrimenti viene sostituita solo la prima occorrenza nella stringa

--- components/ValidateFunction.mjs
+++ components/ValidateFunction.mjs
@@ -1,39 +1,47 @@
 export function ConvertFunction(value){
+    console.log("\n\nnuova")
+    console.log(value);
     //traduci left e right
-    value = value.replace('\\left', '(');
-    value = value.replace('\\right', ')');
+    value = value.replaceAll('\\left', '(');
+    value = value.replaceAll('\\right', ')');
+    console.log("left e right:"+value);
 
     //traduce le funzioni trigonometriche
-    value = value.replace('\\sin', 'sin');
-    value = value.replace('\\cos', 'cos');
-    value = value.replace('\\tan', 'tan');
-    value = value.replace('\\arcsin', 'asin');
-    value = value.replace('\\arccos', 'acos');
-    value = value.replace('\\arctan', 'atan');
-    value = value.replace('\\sinh', 'sinh');
-    value = value.replace('\\cosh', 'cosh');
-    value = value.replace('\\tanh', 'tanh');
+    value = value.replaceAll('\\sin', 'sin');
+    value = value.replaceAll('\\cos', 'cos');
+    value = value.replaceAll('\\tan', 'tan');
+    value = value.replaceAll('\\arcsin', 'asin');
+    value = value.replaceAll('\\arccos', 'acos');
+    value = value.replaceAll('\\arctan', 'atan');
+    value = value.replaceAll('\\sinh', 'sinh');
+    value = value.replaceAll('\\cosh', 'cosh');
+    value = value.replaceAll('\\tanh', 'tanh');
 
     //traduce le funzioni logaritmiche
-    value = value.replace('\\log', 'log');
+    value = value.replaceAll('\\log', 'log');
 
     //traduce le funzioni esponenziali
-    value = value.replace('\\exp', 'exp');
+    value = value.replaceAll('\\exp', 'exp');
 
     //traduce le funzioni di base
-    value = value.replace('\\sqrt', 'sqrt');
+    value = value.replaceAll('\\sqrt', 'sqrt');
 
     //traduce \frac
     value = SubstituteFrac(value);
 
+    //traduce il valore assoluto
+    value = SubstituteAbs(value);
+    console.log("sostituiti:"+value);
+
+
     //cambia le parentesi graffe con le tonde
-    value = value.replace('{', '(');
-    value = value.replace('}', ')');
+    value = value.replaceAll('{', '(');
+    value = value.replaceAll('}', ')');
     //cambia i backslash con gli slash
-    value = value.replace('\\', '/');
+    value = value.replaceAll('\\', '/');
 
     //logga la formula dopo le modifiche
-    //console.log(value);
+    console.log("modificata:"+value);
 
     //prova a compilare la formula, per vedere se è corretta
     var compile = require('interval-arithmetic-eval');
@@ -98,6 +106,41 @@ function SubstituteFrac(value){
       //sostituisci
       value = value.substring(0,start1) + '(' + value.substring(start1+6,end1) + ')/(' + value.substring(start2+1,end2) +')'+ value.substring(end2+1);        
       start1 = value.indexOf("\\frac{");
+    }
+    return value;
+}
+
+function SubstituteAbs(value){
+    //quando trovi aperta tonda + |, vai a | + chiusa tonda
+    //e sostituisci con abs()
+
+    //posizione dell'inizio del primo blocco
+    var start1 = value.indexOf("(|");
+    while (start1>-1){
+      //posizione della fine del primo blocco
+      var end1;
+
+      var diff=1;
+      for (end1=start1+2;end1<value.length;end1++){
+        if (value[end1]=='('){
+          diff++;
+        }
+        if (value[end1]==')'){
+          diff--;
+        }
+        if (diff==0){
+            if (end1<value.length-1 && value[end1+1]=='|'){
+                break;
+            }
+        }
+      }
+      if (end1>=value.length){
+        break;
+      }
+
+      //sostituisci
+      value = value.substring(0,start1) + 'abs(' + value.substring(start1+2,end1) + ')'+ value.substring(end1+2);        
+      start1 = value.indexOf("(|");
     }
     return value;
 }
