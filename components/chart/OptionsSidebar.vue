5a19b484ebff30bcda8ddfe9246070655065a260
Mattia541993 <mattia.ducci93@gmail.com>
2023-03-07 00:27:31 +0100

    WIP inizio gestione sidebar con un'opzione di prova

--- components/chart/OptionsSidebar.vue
+++ components/chart/OptionsSidebar.vue
@@ -1,12 +1,41 @@
 <template>
-  <b-sidebar backdrop :id="sidebarId"> </b-sidebar>
+  <b-sidebar backdrop :id="sidebarId" title="Opzioni">
+    <div class="px-3 py-2">
+      <!-- TODO: inserire vari componenti dinamici-->
+      <!-- https://blog.codeminer42.com/how-to-use-dynamic-components-in-vue/ -->
+      <component
+        v-for="(option, index) of customizableOptionsData"
+        :key="index"
+        v-bind="option"
+        :is="mapTypeComponent[option.type]"
+      ></component>
+    </div>
+  </b-sidebar>
 </template>
 
 <script>
+import OptionXDomain from "../option/XDomain.vue";
+
 export default {
+  components: { OptionXDomain },
   props: {
-    sidebarId: String,
-    required: true,
+    sidebarId: {
+      type: String,
+      required: true,
+    },
+    customizableOptionsData: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  computed: {
+    mapTypeComponent() {
+      return {
+        xDomain: OptionXDomain,
+      };
+    },
   },
 };
 </script>
---
+++ components/option/XDomain.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      xMin: 0,
+      xMax: 0,
+      step: 0,
+    };
+  },
+};
+</script>
